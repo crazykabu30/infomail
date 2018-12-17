b9ba48e0efad4b4130be33ea06d28e6ecc12018b
crazykabu30 <yykyyk30@gmail.com>
2018-12-18 00:28:50 +0900

    create MailDbTest

---
+++ test/db/mailDbTest.php
@@ -0,0 +1,12 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+use infomail\db\MailDb;
+
+include 'vendor/autoload.php';
+include 'infomail/src/db/MailDb.php';
+
+class MailDbTest extends TestCase 
+{
+
+}

aabd9ec56cf515ea31ae1aab95458d3d83a2697e
crazykabu30 <yykyyk30@gmail.com>
2018-12-18 00:34:31 +0900

    del some lines

--- config/pdo.php
+++ config/pdo.php
@@ -4,10 +4,6 @@ namespace infomail\config;
 
 class Pdo 
 {
-	// protected $user = 'ka-kikaku';
-	// protected $pass = 'kaka7890';
-	// protected $pdo = 'mysql:dbname=ka-kikaku_crew_report;host=ql465.db.sakura.ne.jp';
-	// mysql -u ka-kikaku -p ka-kikaku_crew_report -h ql465.db.sakura.ne.jp
 	protected $user = 'user';
 	protected $pass = 'p';
 	protected $pdo = 'mysql:dbname=crew_report;host=localhost';

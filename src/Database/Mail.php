21093506295a8ffd4522908a54013ea8a96bff34
crazykabu30 <yykyyk30@gmail.com>
2018-12-24 23:57:52 +0900

    update Mail.php 件名・本文の登録を実装しました。

--- src/Database/Mail.php
+++ src/Database/Mail.php
@@ -8,9 +8,20 @@ class Mail
 	 * @var array list of send to addresses
 	 */
 	public $sendTo;
+	/**
+	 * @var string title
+	 */
+	public $title;
+	/**
+	 * @var string content-body
+	 */
+	public $body;
+	// constructor function
 	public function __construct()
 	{
 		$this->sendTo = array();
+		$this->title = '';
+		$this->body = '';
 	}
 	/**
 	 * 送信先を登録する
@@ -31,6 +42,44 @@ class Mail
 	public function getSendTo()
 	{
 		return $this->sendTo;
+	}
+	/**
+	 * タイトルを登録する
+	 * 
+	 * @var string
+	 */
+	public function setTitle($val)
+	{
+		$this->title = $val;
+		return true;
+	}
+	/**
+	 * タイトルを確認する
+	 *
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+	/**
+	 * 本文を登録する
+	 * 
+	 * @var string
+	 */
+	public function setBody($val)
+	{
+		$this->body = $val;
+		return true;
+	}
+	/**
+	 * 本文を確認する
+	 *
+	 * @return string
+	 */
+	public function getBody()
+	{
+		return $this->body;
 	}
 	/**
 	 * メールを送る
--- test/Database/MailTest.php
+++ test/Database/MailTest.php
@@ -19,4 +19,26 @@ class MailTest extends TestCase
 		$this->assertTrue($mail->setSendTo($sendto));
 		$this->assertEquals(array('poshette24@gmail.com','yykyyk30@gmail.com'),$mail->getSendTo());
 	}
+	public function testSetTitle()
+	{
+		$mail = new Mail();
+		$this->assertEquals('',$mail->getTitle());
+		$title = 'title1';
+		$this->assertTrue($mail->setTitle($title));
+		$this->assertEquals('title1',$mail->getTitle());
+		$title = 'title2';
+		$this->assertTrue($mail->setTitle($title));
+		$this->assertEquals('title2',$mail->getTitle());
+	}
+	public function testSetBody()
+	{
+		$mail = new Mail();
+		$this->assertEquals('',$mail->getBody());
+		$title = 'body1';
+		$this->assertTrue($mail->setBody($title));
+		$this->assertEquals('body1',$mail->getBody());
+		$title = 'body2';
+		$this->assertTrue($mail->setBody($title));
+		$this->assertEquals('body2',$mail->getBody());
+	}
 }
